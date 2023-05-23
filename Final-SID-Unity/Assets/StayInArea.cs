fcf377c34b508f3fbb8fac64694af6bbce410a2c
Tomas Alvarez <tomas1alvares@gmail.com>
2023-05-23 01:17:34 -0500

    Saveee

--- Final-SID-Unity/Assets/StayInArea.cs
+++ Final-SID-Unity/Assets/StayInArea.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class StayInArea : MonoBehaviour
 {
-    public Vector2 minBoundary; // Minimum boundary coordinates (bottom-left corner of the area)
-    public Vector2 maxBoundary; // Maximum boundary coordinates (top-right corner of the area)
+    public Vector2 minBoundary; // Coordenadas mínimas del límite (esquina inferior izquierda del área)
+    public Vector2 maxBoundary; // Coordenadas máximas del límite (esquina superior derecha del área)
 
-    private void LateUpdate()
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        Vector3 clampedPosition = transform.position; // Get the current position of the GameObject
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-        // Clamp the position within the defined area boundaries
+    private void FixedUpdate()
+    {
+        Vector2 clampedPosition = rb.position; // Obtener la posición actual del Rigidbody
+
+        // Clamp la posición dentro de los límites del área definidos
         clampedPosition.x = Mathf.Clamp(clampedPosition.x, minBoundary.x, maxBoundary.x);
         clampedPosition.y = Mathf.Clamp(clampedPosition.y, minBoundary.y, maxBoundary.y);
 
-        // Update the position of the GameObject
-        transform.position = clampedPosition;
+        // Actualizar la posición del Rigidbody
+        rb.position = clampedPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Cambiar la dirección de la velocidad tanto en x como en y
+            Vector2 newVelocity = -rb.velocity;
+            rb.velocity = newVelocity;
+        }
     }
 }
-
