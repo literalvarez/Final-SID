9d4cfc1c09225e1de72ed37c8794898030271730
Tomas Alvarez <tomas1alvares@gmail.com>
2023-05-22 19:10:39 -0500

    Updating Menu

--- Final-SID-Unity/Assets/Scripts/MatchmakingManager.cs
+++ Final-SID-Unity/Assets/Scripts/MatchmakingManager.cs
@@ -3,18 +3,29 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.Events;
+using TMPro;
 
 public class MatchmakingManager : MonoBehaviourPunCallbacks
 {
     private const int MaxPlayersPerRoom = 2; // Número máximo de jugadores por habitación
-    private const string GameSceneName = "Game"; // Reemplaza con el nombre de tu escena de juego
+    private const string GameSceneName = "Juego"; // Reemplaza con el nombre de tu escena de juego
 
     public Button matchmakingButton; // Referencia al botón de matchmaking en la interfaz
 
     public UnityEvent DoStartMatchmaking;
     public UnityEvent DoConnectedToMaster;
+    public UnityEvent DoJoinRandomFailed;
+    public UnityEvent DoCreateRoom;
+
+    public UnityEvent DoEnterRoom;
+
+
+    //public GameObject textMeshProObject;
+    public TextMeshProUGUI playersConnected;
+    int players;
     private void Start()
     {
+        //playersConnected = textMeshProObject.GetComponent<TextMeshProUGUI>();
         // Deshabilitar el botón hasta que el cliente esté conectado y listo
         matchmakingButton.interactable = false;
 
@@ -37,17 +48,23 @@ public class MatchmakingManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
+        DoJoinRandomFailed.Invoke();
         Debug.Log("No se encontraron habitaciones aleatorias. Creando una nueva habitación...");
         CreateRoom();
     }
 
     private void CreateRoom()
     {
+        DoCreateRoom.Invoke();
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayersPerRoom });
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
+        DoEnterRoom.Invoke();
+        players = PhotonNetwork.CurrentRoom.PlayerCount;
+        playersConnected.text = "Players " + players.ToString() + "/2";
+
         Debug.Log("Nuevo jugador unido a la sala.");
         Debug.Log("Jugadores en la sala: " + PhotonNetwork.CurrentRoom.PlayerCount);
         if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayersPerRoom)
@@ -60,6 +77,10 @@ public class MatchmakingManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
+        DoEnterRoom.Invoke();
+        players = PhotonNetwork.CurrentRoom.PlayerCount;
+        playersConnected.text = "Players " + players.ToString() + "/2";
+
         Debug.Log("Se ha unido a una habitación.");
         Debug.Log("Jugadores en la sala: " + PhotonNetwork.CurrentRoom.PlayerCount);
         if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayersPerRoom)
