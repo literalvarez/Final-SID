9d0d2fa8d27ac3eb68a51fa21704d3cdc9394451
Tomas Alvarez <tomas1alvares@gmail.com>
2023-05-23 02:28:20 -0500

    LAST TRY

--- Final-SID-Unity/Assets/HockeyDisk.cs
+++ Final-SID-Unity/Assets/HockeyDisk.cs
@@ -1,9 +1,11 @@
 using Photon.Pun;
 using UnityEngine;
 
-public class HockeyDisk : MonoBehaviourPun
+public class HockeyDisk : MonoBehaviourPun, IPunObservable
 {
     private Rigidbody2D rigidbody2D;
+    private Vector3 networkPosition;
+    private Quaternion networkRotation;
 
     private void Awake()
     {
@@ -12,6 +14,33 @@ public class HockeyDisk : MonoBehaviourPun
         if (!photonView.IsMine)
         {
             rigidbody2D.isKinematic = true;
+            GetComponent<CircleCollider2D>().enabled = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+        {
+            // Smoothly interpolate to the network position and rotation
+            transform.position = Vector3.Lerp(transform.position, networkPosition, Time.fixedDeltaTime * 5f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.fixedDeltaTime * 5f);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // Send the disk's position and rotation to the network
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // Receive the disk's position and rotation from the network
+            networkPosition = (Vector3)stream.ReceiveNext();
+            networkRotation = (Quaternion)stream.ReceiveNext();
         }
     }
 
@@ -30,5 +59,15 @@ public class HockeyDisk : MonoBehaviourPun
         {
             // Handle player collision logic here
         }
+
+        // Synchronize the collision information across the network
+        photonView.RPC("SyncCollision", RpcTarget.Others, collision.gameObject.tag);
+    }
+
+    [PunRPC]
+    private void SyncCollision(string collidedTag)
+    {
+        // Handle collision logic on other clients
+        // You can access the collidedTag to determine the collision type and handle it accordingly
     }
 }
