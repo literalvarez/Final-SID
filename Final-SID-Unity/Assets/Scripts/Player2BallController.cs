4a66a04442195157b2ea6e5b592458815a477c43
Ricardo <87912063+JuanRicardo6@users.noreply.github.com>
2023-05-23 18:07:23 -0500

    gcfhgcfghfghf

--- Final-SID-Unity/Assets/Scripts/Player2BallController.cs
+++ Final-SID-Unity/Assets/Scripts/Player2BallController.cs
@@ -9,45 +9,41 @@ public class Player2BallController : MonoBehaviourPun, IPunObservable
     [SerializeField] float minX, maxX;
     [SerializeField] float minY, maxY;
     [SerializeField] Vector3 initialPositionPlayer2;
-    public float pushForce = 5f; // Fuerza de empuje a aplicar a la bola 3
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
     private void Start()
     {
         rb.position = initialPositionPlayer2;
     }
-
     private void Update()
     {
         if (photonView.IsMine)
         {
             // Mover la bola según la posición del mouse del jugador local
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Obtener la posición del mouse
 
             // Restringir el rango de movimiento en el eje X
-            if (mousePosition.x < minX)
+            if (mousePosition.x < minX) // minX es el límite derecho del rango permitido
             {
                 mousePosition.x = minX;
             }
-            else if (mousePosition.x > maxX)
+            else if (mousePosition.x > maxX) // maxX es el límite izquierdo del rango permitido
             {
                 mousePosition.x = maxX;
             }
 
             // Restringir el rango de movimiento en el eje Y
-            if (mousePosition.y < minY)
+            if (mousePosition.y < minY) // minY es el límite inferior del rango permitido
             {
                 mousePosition.y = minY;
             }
-            else if (mousePosition.y > maxY)
+            else if (mousePosition.y > maxY) // maxY es el límite superior del rango permitido
             {
                 mousePosition.y = maxY;
             }
-
             // Mover la bola según la posición del mouse
             rb.MovePosition(mousePosition);
         }
@@ -57,23 +53,6 @@ public class Player2BallController : MonoBehaviourPun, IPunObservable
             // Sincronizar la posición y rotación de la bola con la información recibida de la red
             transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * interpolationFactor);
             transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * interpolationFactor);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Vector2 direction = collision.GetContact(0).point - (Vector2)transform.position;
-            direction.Normalize();
-
-            // Aplicar la fuerza de empuje a la bola 3
-            GameObject bola3 = GameObject.FindWithTag("Ball3");
-            if (bola3 != null)
-            {
-                Rigidbody2D rbBola3 = bola3.GetComponent<Rigidbody2D>();
-                rbBola3.AddForce(direction * pushForce, ForceMode2D.Impulse);
-            }
         }
     }
 
@@ -93,6 +72,5 @@ public class Player2BallController : MonoBehaviourPun, IPunObservable
         }
     }
 }
-
 
 
