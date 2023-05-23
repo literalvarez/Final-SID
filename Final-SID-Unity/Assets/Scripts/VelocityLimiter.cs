c324330eb69f23c4f9136a5e1ffff0fc41f0c0ec
Tomas Alvarez <tomas1alvares@gmail.com>
2023-05-23 01:30:34 -0500

    Velocity Limiter Bounce Update

--- Final-SID-Unity/Assets/Scripts/VelocityLimiter.cs
+++ Final-SID-Unity/Assets/Scripts/VelocityLimiter.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VelocityLimiter : MonoBehaviour
 {
     public float maxVelocity = 5f; // Maximum velocity magnitude
+    public float reflectionForce = 1f; // Force applied to the puck upon collision
 
+    private Vector2 direction; // Current direction of the puck
     private Rigidbody2D rb;
 
     private void Awake()
@@ -13,24 +13,30 @@ public class VelocityLimiter : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        // Randomly set the initial direction of the puck
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+
+    private void Update()
     {
         // Limit the velocity magnitude if it exceeds the maximum
         if (rb.velocity.magnitude > maxVelocity)
         {
             rb.velocity = rb.velocity.normalized * maxVelocity;
         }
+
+        // Move the puck based on its current direction and speed
+        rb.velocity = direction * maxVelocity;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Calculate the combined velocity of both objects involved in the collision
-        Vector2 combinedVelocity = rb.velocity + collision.relativeVelocity;
+        // Calculate the reflection direction based on the collision normal
+        Vector2 reflectionDirection = Vector2.Reflect(direction, collision.contacts[0].normal).normalized;
 
-        // Limit the combined velocity if it exceeds the maximum
-        if (combinedVelocity.magnitude > maxVelocity)
-        {
-            rb.velocity = combinedVelocity.normalized * maxVelocity;
-        }
+        // Apply the reflection force to the puck upon collision
+        direction = reflectionDirection * reflectionForce;
     }
 }
