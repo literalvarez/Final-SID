910671a5ce0a16d7d7d639d462ce6bcfbc4fc4c3
Tomas Alvarez <tomas1alvares@gmail.com>
2023-05-23 00:50:00 -0500

    saveee

--- Final-SID-Unity/Assets/Scripts/VelocityLimiter.cs
+++ Final-SID-Unity/Assets/Scripts/VelocityLimiter.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class VelocityLimiter : MonoBehaviour
+public class VelocityLimiter : MonoBehaviourPun, IPunObservable
 {
     public float maxVelocity = 5f; // Maximum velocity magnitude
 
     private Rigidbody2D rb;
+
+    private Vector3 networkPosition;
+    private Quaternion networkRotation;
 
     private void Awake()
     {
@@ -15,6 +19,10 @@ public class VelocityLimiter : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float interpolationFactor = 5f; // Ajusta este valor según tus necesidades
+        transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * interpolationFactor);
+        transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * interpolationFactor);
+
         // Limit the velocity magnitude if it exceeds the maximum
         if (rb.velocity.magnitude > maxVelocity)
         {
@@ -31,6 +39,22 @@ public class VelocityLimiter : MonoBehaviour
         if (combinedVelocity.magnitude > maxVelocity)
         {
             rb.velocity = combinedVelocity.normalized * maxVelocity;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // Enviar la posición y rotación de la bola al resto de los jugadores
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // Recibir la posición y rotación de la bola desde el jugador propietario
+            networkPosition = (Vector3)stream.ReceiveNext();
+            networkRotation = (Quaternion)stream.ReceiveNext();
         }
     }
 }
